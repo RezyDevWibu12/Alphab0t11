040cb9107a34ce0d6053feec88a8c79d0041500d
ErozeBotz <92363825+ErozeBotz@users.noreply.github.com>
2021-11-22 19:38:03 +0700

    Update grupbot.js

--- shop/grupbot.js
+++ shop/grupbot.js
@@ -2,14 +2,14 @@
 const gcbotwa = () =>{
 	return`Join Aja Semua Fitur Bot Bisa Digunakan !
 
-1. *Alphabot Support*
-_https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV_
-2. *Alphabot Support 2*
-_https://chat.whatsapp.com/E8NExJwIbhBJYzssfqJNsE_
-3. *Alphabot Support 3*
-_https://chat.whatsapp.com/KCSqHTky1apG7ApePsfiPy_
-4. *Alphabot Support 4* 
-_https://chat.whatsapp.com/KwmvHr7VMFj7r5ry9xmMsU_
+1. *ErozeBot Support*
+_https://chat.whatsapp.com/LNSLUbCXb7S6mGFBFITY76_
+2. *ErozeBot Support 2*
+_https://chat.whatsapp.com/LNSLUbCXb7S6mGFBFITY76_
+3. *ErozeBot Support 3*
+_https://chat.whatsapp.com/LNSLUbCXb7S6mGFBFITY76_
+4. *ErozeBot Support 4* 
+_https://chat.whatsapp.com/LNSLUbCXb7S6mGFBFITY76_
 
 Jika ada link yang ke reset, silahkan hubungi
 owner untuk meminta link yang baru
